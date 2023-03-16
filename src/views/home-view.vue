d7eb30ce5c6af68fddde24d00d8f1910724e14b1
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2023-03-16 15:31:28 +0530

    Update home-view.vue

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -278,8 +278,7 @@
     },
     methods: {
       redirectToLink() {
-        window.location.href = "https://drive.google.com/file/d/1Uzxdpb52DeJ0QmlRLOiXjEisVvipG7bw/view?usp=share_link";
-        download='Resume Anirudh D(6).pdf';
+        window.location.href = "https://drive.google.com/file/d/1Uzxdpb52DeJ0QmlRLOiXjEisVvipG7bw/view?usp=share_link"; _target='none';
       },
     },
   };
