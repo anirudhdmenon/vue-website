e4eb5a2280e5f160fcf3be93ce6ef51e1b4722f1
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2023-03-16 15:24:22 +0530

    Update home-view.vue

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -26,7 +26,7 @@
                               Programming, Python, Full stack web Development and SQL. Looking to start career 
                               as an entry-level software engineer with a reputed firm driven by technology.</p>
         <v-btn title color="#A7121D" dark @click="redirectToLink">About Me</v-btn>
-        <button onclick="window.location.href='https://drive.google.com/file/d/1Uzxdpb52DeJ0QmlRLOiXjEisVvipG7bw/view?usp=share_link'; download='Resume Anirudh D(6).pdf'">About Me</button>
+        <button onclick="window.location.href='https://drive.google.com/file/d/1Uzxdpb52DeJ0QmlRLOiXjEisVvipG7bw/view?usp=share_link'">About Me</button>
 
       </v-col>
       <v-col cols="12" class="padd" id="skills">
